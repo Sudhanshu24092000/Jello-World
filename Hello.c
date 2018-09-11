4c04e148a3e8873a14d46ab3ff948b73356f67b4
Sudhanshu24092000 <43162212+Sudhanshu24092000@users.noreply.github.com>
2018-09-11 10:54:46 +0530

    hello.c

---
+++ Hello.c
@@ -0,0 +1,7 @@
+# Jello-World
+It is written in C language
+#include<stdio.h>
+voidmain()
+{
+printf("Hello World");
+}
--- README.md
+++
@@ -1,6 +0,0 @@
-# Jello-World
-It is written in C language
-{
-printf("Hello world");
-}
-
